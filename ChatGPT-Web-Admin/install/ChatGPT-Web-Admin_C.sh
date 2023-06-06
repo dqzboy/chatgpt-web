342e07b52610deadaa54baaecb5d7eb7ccfeb122
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-06-06 16:08:12 +0800

    Adjust the Nginx reverse proxy configuration.

--- ChatGPT-Web-Admin/install/ChatGPT-Web-Admin_C.sh
+++ ChatGPT-Web-Admin/install/ChatGPT-Web-Admin_C.sh
@@ -628,8 +628,7 @@ server {
         proxy_pass http://localhost:3002;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;        
-        proxy_set_header X-Nginx-Proxy true;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_buffering off;
         proxy_redirect off;
     }
