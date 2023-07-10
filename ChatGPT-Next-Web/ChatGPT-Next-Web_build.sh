bb71d6a0e70c098c727ba151aadde197336eb4a3
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-07-10 15:56:42 +0800

    Update ChatGPT-Next-Web_build.sh

--- ChatGPT-Next-Web/ChatGPT-Next-Web_build.sh
+++ ChatGPT-Next-Web/ChatGPT-Next-Web_build.sh
@@ -33,6 +33,93 @@ width=75
 padding=$((($width - ${#text}) / 2))
 
 
+
+function CHECK_OS() {
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+else
+    echo "无法确定发行版"
+    exit 1
+fi
+
+
+# 根据发行版选择存储库类型
+case "$ID" in
+    "centos")
+        repo_type="centos"
+        ;;
+    "debian")
+        repo_type="debian"
+        ;;
+    "rhel")
+        repo_type="rhel"
+        ;;
+    "ubuntu")
+        repo_type="ubuntu"
+        ;;
+    "opencloudos")
+        repo_type="centos"
+        ;;
+    "rocky")
+        repo_type="centos"
+        ;;
+    *)
+        WARN "此脚本暂不支持您的系统: $ID"
+        exit 1
+        ;;
+esac
+
+echo "------------------------------------------"
+echo "系统发行版: $NAME"
+echo "系统版本: $VERSION"
+echo "系统ID: $ID"
+echo "系统ID Like: $ID_LIKE"
+echo "------------------------------------------"
+}
+
+function CHECKFIRE() {
+SUCCESS "Firewall && SELinux detection."
+
+# Check if firewall is enabled
+systemctl stop firewalld &> /dev/null
+systemctl disable firewalld &> /dev/null
+systemctl stop iptables &> /dev/null
+systemctl disable iptables &> /dev/null
+ufw disable &> /dev/null
+INFO "Firewall has been disabled."
+
+# Check if SELinux is enforcing
+if [[ "$repo_type" == "centos" || "$repo_type" == "rhel" ]]; then
+    if sestatus | grep "SELinux status" | grep -q "enabled"; then
+        WARN "SELinux is enabled. Disabling SELinux..."
+        setenforce 0
+        sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+        INFO "SELinux is already disabled."
+    else
+        INFO "SELinux is already disabled."
+    fi
+fi
+}
+
+function INSTALL_PACKAGE(){
+PACKAGES_APT="lsof jq wget postfix mailutils git"
+PACKAGES_YUM="lsof jq wget postfix yum-utils mailx s-nail git"
+# 检查命令是否存在
+if command -v yum >/dev/null 2>&1; then
+    SUCCESS "安装系统必要组件"
+    yum -y install $PACKAGES_YUM --skip-broken &>/dev/null
+    systemctl restart postfix &>/dev/null
+elif command -v apt-get >/dev/null 2>&1; then
+    SUCCESS "安装系统必要组件"
+    apt-get install -y $PACKAGES_APT --ignore-missing &>/dev/null
+    systemctl restart postfix &>/dev/null
+else
+    WARN "无法确定可用的包管理器"
+    exit 1
+fi
+}
+
+
 function DL() {
 SUCCESS "脚本下载"
 printf "%*s\033[31m%s\033[0m%*s\n" $padding "" "$text" $padding ""
@@ -40,20 +127,15 @@ SUCCESS "执行完成"
 
 read -e -p "请选择你的服务器网络环境[国外1/国内2]： " NETWORK
 if [ ${NETWORK} == 1 ];then
-    if [ -f /etc/redhat-release ]; then
-        INFO "《This is CentOS.》"
+    if [ "$repo_type" = "centos" ] || [ "$repo_type" = "rhel" ]; then
+        INFO "《This is $repo_type.》"
         SUCCESS "系统环境检测中，请稍等..."
-        bash -c "$(wget -q -O- https://raw.githubusercontent.com/dqzboy/ChatGPT/main/ChatGPT-Next-Web/install/ChatGPT-Next-Web_C.sh)"
-    elif [ -f /etc/lsb-release ]; then
-        if grep -q "DISTRIB_ID=Ubuntu" /etc/lsb-release; then
-            INFO "《This is Ubuntu.》"
-            SUCCESS "系统环境检测中，请稍等..."
-            systemctl restart systemd-resolved
-            bash -c "$(wget -q -O- https://raw.githubusercontent.com/dqzboy/ChatGPT/main/ChatGPT-Next-Web/install/ChatGPT-Next-Web_U.sh)"
-        else
-            echo "Unknown Linux distribution."
-            exit 1
-        fi
+        bash -c "$(wget -q -O- https://raw.githubusercontent.com/dqzboy/ShellProject/main/ChatGPT/ChatGPT-Next-Web/install/ChatGPT-Next-Web_C.sh)"
+    elif [ "$repo_type" == "ubuntu" ] || [ "$repo_type" == "debian" ]; then
+        INFO "《This is $repo_type.》"
+        SUCCESS "系统环境检测中，请稍等..."
+        systemctl restart systemd-resolved
+        bash -c "$(wget -q -O- https://raw.githubusercontent.com/dqzboy/ShellProject/main/ChatGPT/ChatGPT-Next-Web/install/ChatGPT-Next-Web_U.sh)"
     else
         echo "Unknown Linux distribution."
         exit 2
@@ -62,13 +144,13 @@ elif [ ${NETWORK} == 2 ];then
         if [ -f /etc/redhat-release ]; then
         INFO "《This is CentOS.》"
         SUCCESS "系统环境检测中，请稍等..."
-        bash -c "$(wget -q -O- https://ghproxy.com/https://raw.githubusercontent.com/dqzboy/ChatGPT/main/ChatGPT-Next-Web/install/ChatGPT-Next-Web_C.sh)"
+        bash -c "$(wget -q -O- https://ghproxy.com/https://raw.githubusercontent.com/dqzboy/ShellProject/main/ChatGPT/ChatGPT-Next-Web/install/ChatGPT-Next-Web_C.sh)"
     elif [ -f /etc/lsb-release ]; then
         if grep -q "DISTRIB_ID=Ubuntu" /etc/lsb-release; then
             INFO "《This is Ubuntu.》"
             SUCCESS "系统环境检测中，请稍等..."
             systemctl restart systemd-resolved
-            bash -c "$(wget -q -O- https://ghproxy.com/https://raw.githubusercontent.com/dqzboy/ChatGPT/main/ChatGPT-Next-Web/install/ChatGPT-Next-Web_U.sh)"
+            bash -c "$(wget -q -O- https://ghproxy.com/https://raw.githubusercontent.com/dqzboy/ShellProject/main/ChatGPT/ChatGPT-Next-Web/install/ChatGPT-Next-Web_U.sh)"
         else
             echo "Unknown Linux distribution."
             exit 1
@@ -81,4 +163,10 @@ else
    ERROR "Parameter Error"
 fi
 }
+function main() {
+CHECK_OS
+CHECKFIRE
+INSTALL_PACKAGE
 DL
+}
+main
