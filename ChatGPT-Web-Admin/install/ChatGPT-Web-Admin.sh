55af6d0ce7b9acb68c16bbbb9b05c052dc3c497e
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-10-20 15:47:26 +0800

    Update ChatGPT-Web-Admin.sh

--- ChatGPT-Web-Admin/install/ChatGPT-Web-Admin.sh
+++ ChatGPT-Web-Admin/install/ChatGPT-Web-Admin.sh
@@ -343,7 +343,6 @@ case "$choice" in
   y|Y )
     read -e -p "请输入 MongoDB 用户名：" MONGODB_USERNAME
     read -e -p "请输入 MongoDB 密码：" MONGODB_PASSWORD
-    echo
     mongosh <<EOF >/dev/null 2>&1
     use admin
     db.createUser({
